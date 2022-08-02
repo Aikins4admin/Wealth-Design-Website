0fe6f8827139d378fbb24169543ea29e077dec76
Edudzi Aikins <aikinsedudzi@gmail.com>
2022-08-02 13:10:28 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if(window.pageYOffset > 200) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
